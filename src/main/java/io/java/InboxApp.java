1a9ed0ef03bf6864fb70aff786dc59398d8aef67
anilanshu <anshu20126@gmail.com>
2023-01-19 23:15:12 +0530

    inbox app login using spring boot

---
+++ src/main/java/io/java/InboxApp.java
@@ -0,0 +1,23 @@
+package io.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InboxApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxApp.class, args);
+	}
+
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
+	}
+}
